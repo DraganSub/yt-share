7b6a98507849f0d96d75a4bb76a54c9a733aa0c0
Dragan <dragan.subi7@gmail.com>
2023-06-17 22:43:16 +0200

    oso

--- server/index.js
+++ server/index.js
@@ -2,57 +2,57 @@ const express = require('express')
 const { WebSocketServer } = require("ws")
 const cors = require('cors');
 
-const socketServer = new WebSocketServer({ port: 443 })
+const socketServer = new WebSocketServer({ port: 8080 })
 const app = express()
 const port = 3001;
 
 app.use(cors())
 app.listen(port, () => {
-    console.log(`player listening on port ${port}`)
+  console.log(`player listening on port ${port}`)
 })
 let currentTime = 0;
 
 socketServer.on("connection", ws => {
-    console.log("new connection");
-    ws.send(JSON.stringify({
-        message: "connection-established"
-    }));
-    console.log("current currentTime", currentTime)
-    ws.send(JSON.stringify({
-        message: "currentTime",
-        currentTime: currentTime
-    }))
+  console.log("new connection");
+  ws.send(JSON.stringify({
+    message: "connection-established"
+  }));
+  console.log("current currentTime", currentTime)
+  ws.send(JSON.stringify({
+    message: "currentTime",
+    currentTime: currentTime
+  }))
 
-    ws.on("message", (event) => {
-        console.log("message come", JSON.parse(event))
-        const data = JSON.parse(event.toString('utf8'));
-        if (data.message === "playVideo") {
-            console.log("message send")
-            socketServer.clients.forEach(client => {
-                client.send(
-                    JSON.stringify({
-                        message: "play",
-                    })
-                )
-            })
-        } else if (data.message === "stopVideo") {
-            socketServer.clients.forEach(client => {
-                client.send(
-                    JSON.stringify({
-                        message: "stop",
-                    })
-                )
-            })
-        } else if (data.message === "seekSeconds") {
-            if (currentTime < data.currentTime) {
-                console.log("reset currenttime to bigger value")
-                currentTime = data.currentTime;
-            }
-        }
+  ws.on("message", (event) => {
+    console.log("message come", JSON.parse(event))
+    const data = JSON.parse(event.toString('utf8'));
+    if (data.message === "playVideo") {
+      console.log("message send")
+      socketServer.clients.forEach(client => {
+        client.send(
+          JSON.stringify({
+            message: "play",
+          })
+        )
+      })
+    } else if (data.message === "stopVideo") {
+      socketServer.clients.forEach(client => {
+        client.send(
+          JSON.stringify({
+            message: "stop",
+          })
+        )
+      })
+    } else if (data.message === "seekSeconds") {
+      if (currentTime < data.currentTime) {
+        console.log("reset currenttime to bigger value")
+        currentTime = data.currentTime;
+      }
+    }
 
-    })
+  })
 
-    ws.on("close", () => console.log("client disconnected"));
+  ws.on("close", () => console.log("client disconnected"));
 
 
 })
