45855311650899f0a8d9d449473bdbe1d36005fd
Dragan <dragan.subi7@gmail.com>
2023-06-17 13:47:02 +0200

    add server logic

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = 3001;
+
+
+io.on('connection', (socket) => {
+  console.log(`New client connected: ${socket.id}`);
+
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+    console.log(`Client ${socket.id} joined room ${room}`);
+  });
+
+  socket.on('playVideo', (data) => {
+    io.to(data.room).emit('playVideo', data);
+  });
+
+  socket.on('pauseVideo', (data) => {
+    io.to(data.room).emit('pauseVideo', data);
+  });
+
+  socket.on('seekVideo', (data) => {
+    io.to(data.room).emit('seekVideo', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Client disconnected: ${socket.id}`);
+  });
+});
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
